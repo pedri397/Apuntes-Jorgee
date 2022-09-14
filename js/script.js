05e3fd376b02a621224a5110bcc375bb2ea5f04a
pedro@DESKTOP-KS93E90 <pedri397gmail.com>
2022-09-14 19:26:08 +0200

    commit inicial

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+//prompt = sirve para pedir al usuario que introduzca un numero, se le pone 
+//parseInt para que convierta el dato en entero
+
+let n1 = parseInt(prompt("Introduce un numero"))
+let n2 = parseInt(prompt("Introduce un numero"))
+
+alert(n1+n2)
+
+//typeof = para saber de que tipo es el dato
