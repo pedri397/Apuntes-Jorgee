85bbb238c78e886c843d0cf58dd9f15872c3ade0
pedro@DESKTOP-KS93E90 <pedri397gmail.com>
2022-09-16 21:19:03 +0200

    commit2

---
+++ js/Ejercicio8.js
@@ -0,0 +1,12 @@
+
+// 8.Realizar un programa que “implemente” el funcionamiento de un Cajero Automático, con las siguientes características:
+// a.Las operaciones que se podrán realizar son:
+
+// MENU DEL CAJERO
+// 1.Sacar dinero.
+// 2.Ingresar dinero.
+// 3.Consultar saldo.
+// 4.Cambiar Contraseña.
+// 5.Salir
+
+
---
+++ js/Function.js
@@ -0,0 +1,11 @@
+//Funciones y constantes
+
+// function sumar(n1,n2){
+//     return n1+n2
+// }
+
+const sumar = (n1,n2)=>{ 
+ return n1+n2
+}
+
+console.log(sumar(7,27))
